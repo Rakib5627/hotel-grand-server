7df29caa40ffbf41a9595c04aef08fefaf076123
Rakibul Islam <mdrakib.5627@gmail.com>
2023-11-08 08:26:36 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app =express();
 const port = process.env.PORT || 5000 ;
 
-app.use(cors());
+// middlewares
+
+app.use(cors({
+  origin: [
+      'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -26,12 +36,33 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("hotel");
     const roomsCollection = database.collection("rooms");
     // const userCollection = database.collection("user");
-    // const cartCollection = database.collection("cart");
+    const bookingCollection = database.collection("booking");      
+    
+      // auth related api
+    
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      console.log('user for token', user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+      res.cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+      })
+          .send({ success: true });
+  })
+
+  app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log('logging out', user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+  })
 
 
     // ------rooms api-------
@@ -42,7 +73,63 @@ async function run() {
         res.send(result);
     })
 
- 
+    app.get('/rooms/:id', async(req , res) => {
+        const id = req.params.id;  
+        // console.log(id) 
+        const query = {_id : new ObjectId(id)};
+        const result = await roomsCollection.findOne(query);
+        res.send(result);
+    })
+
+
+    
+
+
+
+// -------------------bookings-------------
+
+app.get('/bookings', async (req, res) => {
+    console.log(req.query.email);
+    console.log('token owner info', req.user)
+    if(req.user.email !== req.query.email){
+        return res.status(403).send({message: 'forbidden access'})
+    }
+    let query = {};
+    if (req.query?.email) {
+        query = { email: req.query.email }
+    }
+    const result = await bookingCollection.find(query).toArray();
+    res.send(result);
+})
+
+app.post('/bookings', async (req, res) => {
+    const booking = req.body;
+    console.log(booking);
+    const result = await bookingCollection.insertOne(booking);
+    res.send(result);
+});
+
+// app.patch('/bookings/:id', async (req, res) => {
+//     const id = req.params.id;
+//     const filter = { _id: new ObjectId(id) };
+//     const updatedBooking = req.body;
+//     console.log(updatedBooking);
+//     const updateDoc = {
+//         $set: {
+//             status: updatedBooking.status
+//         },
+//     };
+//     const result = await bookingCollection.updateOne(filter, updateDoc);
+//     res.send(result);
+// })
+
+app.delete('/bookings/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await bookingCollection.deleteOne(query);
+    res.send(result);
+})
+
 
 
 
